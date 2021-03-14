add754d00c9e974b276211e3c525ca9e242d5417
F-Gabriel-Braga <braagaa.inf@hotmail.com>
2021-03-14 00:07:34 -0300

    ADD FUNCTION GENERATE ORDER

--- script/script.js
+++ script/script.js
@@ -1,9 +1,24 @@
 
-let shopping_cart = []
 
-const phone = "558899951396"
-let link = "https://api.whatsapp.com/send?phone=558899951396&text=Ola%20Gabriel%0AQuero%20fazer%20meu%20pedido"
+function generate_order() {
+    const phone = '558899951396'
+    const space = '%20'
+    const enter = '%0A'
+    let link = `https://api.whatsapp.com/send?phone=${phone}&text=Olá, meu pedido de hoje é:${enter}`
 
+    const items = document.querySelectorAll(".box-info-order table tbody tr")
+    for(let i = 0; i < items.length; i++) {
+        const quantity = items[i].children[0].innerText
+        const description = items[i].children[1].innerText
+        let item = `* ${quantity} | ${description}${enter}`
+        link += item
+    }
+    const total = `Total: R$ ${calculate_total()}`
+    link += total
+
+    const elementButton = document.querySelector('#button')
+    elementButton.href = link
+}
 
 function calculate_total() {
     const items = document.querySelectorAll(".box-info-order table tbody tr")
@@ -19,32 +34,30 @@ function calculate_total() {
 function update_table(item) {
     const table = document.querySelector(".box-info-order table")
     const items = document.querySelectorAll(".box-info-order table tbody tr")
-    console.log(items)
-    console.log(items.length)
-    console.log(items.length > 0)
 
-    let not_exist = true
+    let not_exist_item = true
     for(let i = 0; i < items.length; i++) {
         if(items[i].children[1].innerText === item.description) {
+            items[i].children[0].innerHTML = item.quantity
+            items[i].children[2].innerHTML = `R$ ${item.value}`
+            not_exist_item = false
             if(item.quantity === 0) { 
                 items[i].parentNode.removeChild(items[i])
             }
-            items[i].children[0].innerHTML = item.quantity
-            items[i].children[2].innerHTML = `R$ ${item.value}`
-            not_exist = false
             break
         }
     }
     
-    if(not_exist) {
-        let html = "<tr>"
+    if(not_exist_item) {
+        let html = '<tr>'
         html += `<td>${item.quantity}</td>`
         html += `<td>${item.description}</td>`
         html += `<td>R$ ${item.value}</td>`
-        html += "</tr>"
+        html += '</tr>'
         
         table.children[2].innerHTML += html
     }
+
     table.children[3].children[0].children[1].innerText = `R$ ${calculate_total()}`
 
     const items_refresh = document.querySelectorAll(".box-info-order table tbody tr")
@@ -78,15 +91,18 @@ function add_to_shopping_cart(event) {
     const elementBox = event.currentTarget
     const elementQuantity = elementBox.children[0]
     const elementBack = elementBox.children[1]
+    const elementDescription = elementBox.children[3].children[0]
+    const elementValue = elementBox.children[3].children[1].children[0]
 
     const quantity = parseInt(elementQuantity.innerText) + 1
-    if(quantity <= 20) {
+
+    if(quantity <= 10) {
         elementQuantity.innerText = quantity
         elementQuantity.style.display = "block"
         elementBack.style.display = "block"
 
-        const description = elementBox.children[3].children[0].innerText
-        const value = parseFloat(elementBox.children[3].children[1].children[0].innerText.replace(",", "."))
+        const description = elementDescription.innerText
+        const value = parseFloat(elementValue.innerText.replace(",", "."))
 
         const item = {quantity: quantity, description: description, value: (value * quantity)}
         update_table(item)
@@ -98,8 +114,11 @@ function remove_to_shopping_cart(event) {
     const elementBack = event.currentTarget
     const elementBox = elementBack.parentElement
     const elementQuantity = elementBox.children[0]
+    const elementDescription = elementBox.children[3].children[0]
+    const elementValue = elementBox.children[3].children[1].children[0]
 
     const quantity = parseInt(elementQuantity.innerText) - 1
+
     if(quantity > 0) {
         elementQuantity.innerText = quantity
     }
@@ -109,8 +128,8 @@ function remove_to_shopping_cart(event) {
         elementBack.style.display = "none"
     }
 
-    const description = elementBox.children[3].children[0].innerText
-    const value = parseFloat(elementBox.children[3].children[1].children[0].innerText.replace(",", "."))
+    const description = elementDescription.innerText
+    const value = parseFloat(elementValue.innerText.replace(",", "."))
 
     const item = {quantity: quantity, description: description, value: (value * quantity)}
     update_table(item)
